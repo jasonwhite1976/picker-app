9db316ef5572f54504e4e6cdcaf46e5dd4f68751
Jason <jasonwhite1976@yahoo.co.uk>
2017-05-17 19:17:16 +0100

    init

--- app.js
+++ app.js
@@ -22,11 +22,10 @@ const multer = require('multer');
 
 const upload = multer({ dest: path.join(__dirname, 'uploads') });
 
-
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
  */
-dotenv.load({ path: '.env' });
+dotenv.load({ path: '.env.example' });
 
 /**
  * Controllers (route handlers).
