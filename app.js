ede49412d592a9c29b37b532ba6952aad21c44a4
Jason <jasonwhite1976@yahoo.co.uk>
2017-05-18 07:14:19 +0100

    config

--- app.js
+++ app.js
@@ -22,10 +22,14 @@ const multer = require('multer');
 
 const upload = multer({ dest: path.join(__dirname, 'uploads') });
 
+const config_vars = GET /apps/powerful-ridge-94414/releases/V15/config-vars;
+console.log(config_vars);
+
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
  */
-dotenv.load({ path: '.env.example' });
+//dotenv.load({ path: '.env.example' });
+dotenv.Load({ config_vars });
 
 /**
  * Controllers (route handlers).
