dbf65ecdd92d052bfd6f839f65fd3a12abedb155
Jason <jasonwhite1976@yahoo.co.uk>
2017-05-22 21:55:04 +0100

    Delete account js

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,6 @@
 $(document).ready(function() {
     $("#delete-btn").submit(function() {
-        if ($("input[type='submit']").val() == "text 1") {
-            alert("Are you sure?");
-            $("input[type='submit']").val("text 2");
-            return false;
-        }
+        var c = confirm("Click OK to continue");
+        return c;
     });
 });
